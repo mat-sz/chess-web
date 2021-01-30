420a61ba59282546b97af24e74b8c8cd94f0eecc
Mat Sz <contact@matsz.dev>
2021-01-30 14:54:07 +0100

    added board generation

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -2,12 +2,38 @@ import { Store } from 'redux';
 import { ActionModel } from '../types/Models';
 import { SquareState } from '../types/SquareState';
 
+function newBoard(): SquareState[] {
+  const emptyRank = new Array(8).fill(SquareState.EMPTY);
+  const bottomRank = [
+    SquareState.WHITE_ROOK,
+    SquareState.WHITE_KNIGHT,
+    SquareState.WHITE_BISHOP,
+    SquareState.WHITE_KING,
+    SquareState.WHITE_QUEEN,
+    SquareState.WHITE_BISHOP,
+    SquareState.WHITE_KNIGHT,
+    SquareState.WHITE_ROOK,
+  ];
+  const pawnRank = new Array(8).fill(SquareState.WHITE_PAWN);
+
+  return [
+    ...(bottomRank.map(piece => piece | 0x10) as SquareState[]),
+    ...(pawnRank.map(piece => piece | 0x10) as SquareState[]),
+    ...emptyRank,
+    ...emptyRank,
+    ...emptyRank,
+    ...emptyRank,
+    ...pawnRank,
+    ...bottomRank,
+  ];
+}
+
 export interface StateType {
   board: SquareState[];
 }
 
 let initialState: StateType = {
-  board: new Array(64).fill(SquareState.EMPTY),
+  board: newBoard(),
 };
 
 export type StoreType = Store<StateType, ActionModel>;
