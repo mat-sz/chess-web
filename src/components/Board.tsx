77e462e06e767aceb86bd23bd37b1072a5a7474e
Mat Sz <contact@matsz.dev>
2021-02-02 18:14:43 +0100

    moved board square to a separate component

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,34 +1,34 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import clsx from 'clsx';
 
 import { StateType } from '../reducers';
 import { Piece } from './Piece';
 import { selectAction } from '../actions/board';
+import { Square } from './Square';
 
 export const Board: React.FC = () => {
   const board = useSelector((state: StateType) => state.board);
   const selectedIndex = useSelector((state: StateType) => state.selectedIndex);
   const dispatch = useDispatch();
 
+  const onSelect = useCallback(
+    (index: number) => {
+      dispatch(selectAction(index));
+    },
+    [dispatch]
+  );
+
   return (
     <div className="board">
-      {board.map((type, i) => {
-        const black = Math.floor(i / 8) % 2 === i % 2;
-        return (
-          <div
-            key={i}
-            onClick={() => dispatch(selectAction(i))}
-            className={clsx('square', {
-              black: black,
-              white: !black,
-              selected: selectedIndex === i,
-            })}
-          >
-            <Piece type={type} />
-          </div>
-        );
-      })}
+      {board.map((type, i) => (
+        <Square
+          index={i}
+          type={type}
+          selected={selectedIndex === i}
+          onSelect={onSelect}
+        />
+      ))}
     </div>
   );
 };
---
+++ src/components/Square.tsx
@@ -0,0 +1,35 @@
+import React, { useCallback } from 'react';
+import clsx from 'clsx';
+
+import { Piece } from './Piece';
+import { PieceType } from '../types/PieceType';
+
+export interface SquareProps {
+  index: number;
+  type: PieceType;
+  selected: boolean;
+  onSelect: (index: number) => void;
+}
+
+export const Square: React.FC<SquareProps> = ({
+  type,
+  selected,
+  index,
+  onSelect,
+}) => {
+  const onClick = useCallback(() => onSelect(index), [onSelect, index]);
+  const black = Math.floor(index / 8) % 2 === index % 2;
+
+  return (
+    <div
+      onClick={onClick}
+      className={clsx('square', {
+        black,
+        white: !black,
+        selected,
+      })}
+    >
+      <Piece type={type} />
+    </div>
+  );
+};
